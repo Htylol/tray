4b67a3a25048f088e4535e1986594f6f4c580e48
Htylol <dixperm909@yandex.ru>
2024-07-26 22:55:27 +0500

    Updated tray_pp, now translations and icon are not required in the directory

--- example/example_pp.cpp
+++ example/example_pp.cpp
@@ -8,10 +8,12 @@
 #define _(STRING) gettext(STRING)
 
 int main() {
-
   setlocale (LC_ALL, "");
-  bindtextdomain ("tray", getenv("PWD"));
+  bindtextdomain ("tray", "../locales");
   textdomain ("tray");
+
+  std::string icon_name = "../img/gui/portproton_tray.svg";
+  auto icon_path = std::filesystem::current_path() / icon_name;
 
   trays::Menu q;
   q.text_ = _("WINEFILE");
@@ -22,16 +24,10 @@ int main() {
   trays::Menu r;
   r.text_ = _("FORCE EXIT");
 
-  // linux: png, svg
-  std::string icon_name = "portproton_tray.svg";
-
-  // absolute path for linux.
-  auto icon_path = std::filesystem::current_path() / icon_name;
   q.on_click_ = [](auto) { system("bash -c pw_tray_winefile &"); };
   w.on_click_ = [](auto) { system("bash -c pw_tray_taskmgr &"); };
   e.on_click_ = [](auto) { system("bash -c open_changelog &"); };
   r.on_click_ = [](auto) { system("bash -c tray_icon_click_exit"); tray_exit(); };
-
   trays::Tray t{icon_path.string(), {q,w,e,r}};
 
   t.run();
